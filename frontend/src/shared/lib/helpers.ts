b80acd4e75fad9daa280dafd39709abc33a390fa
Andrii-K-17 <andrii.klodchyk@gmail.com>
2025-08-17 17:54:00 +0300

    feat(frontend): add formatDate helper

---
+++ frontend/src/shared/lib/helpers.ts
@@ -0,0 +1,8 @@
+export const formatDate = (date: string | Date): string => {
+  const d = new Date(date)
+  const year = d.getFullYear()
+  const month = (d.getMonth() + 1).toString().padStart(2, '0')
+  const day = d.getDate().toString().padStart(2, '0')
+  
+  return `${year}-${month}-${day}`
+}
