f13d705cb2674441153706495b6ac7695900a515
Andrii-K-17 <andrii.klodchyk@gmail.com>
2025-08-18 17:24:55 +0300

    feat(frontend): add truncateText func into helpers

--- frontend/src/shared/lib/helpers.ts
+++ frontend/src/shared/lib/helpers.ts
@@ -6,3 +6,10 @@ export const formatDate = (date: string | Date): string => {
   
   return `${year}-${month}-${day}`
 }
+
+export const truncateText = (text: string, maxLength: number): string => {
+  if (text.length <= maxLength) {
+    return text;
+  }
+  return text.slice(0, maxLength) + '...';
+}
