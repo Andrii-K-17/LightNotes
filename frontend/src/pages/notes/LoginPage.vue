1a041db295463f5792aa0fc2c02422c445919a06
Andrii-K-17 <andrii.klodchyk@gmail.com>
2025-08-18 17:48:15 +0300

    feat(frontend): add register and login pages

---
+++ frontend/src/pages/notes/LoginPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import LoginForm from '../../entities/session/ui/LoginForm.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold mb-2 text-center dark:text-white">Log in</h1>
+    <p class="text-gray-600 dark:text-gray-300 mb-6 text-center">Welcome back! Please sign in to your account.</p>
+    
+    <LoginForm />
+
+    <p class="text-center text-sm dark:text-neutral-300 pt-4">
+      Don’t have an account? 
+      <router-link to="/register" class="text-sky-500 hover:text-sky-600 hover:underline font-medium">
+        Sign up
+      </router-link>
+    </p>
+  </div>
+</template>
---
+++ frontend/src/pages/notes/RegisterPage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import RegisterForm from '../../entities/session/ui/RegisterForm.vue'
+</script>
+
+<template>
+  <div>
+    <h1 class="text-3xl font-bold mb-2 text-center dark:text-white">Sign up</h1>
+    <p class="text-gray-600 dark:text-gray-300 mb-6 text-center">Create your account to get started.</p>
+    
+    <RegisterForm />
+
+    <p class="text-center text-sm dark:text-neutral-400 pt-4">
+      Already have an account? 
+      <router-link to="/login" class="text-sky-500 hover:text-sky-600 hover:underline font-medium">
+        Sign in
+      </router-link>
+    </p>
+  </div>
+</template>
