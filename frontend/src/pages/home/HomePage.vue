a795d5f1e7720a5c5050fef8d5b0fde081acdd32
Andrii-K-17 <andrii.klodchyk@gmail.com>
2025-08-20 19:35:54 +0300

    refactor(frontend): optimize note creation and sidebar on HomePage

--- frontend/src/pages/home/HomePage.vue
+++ frontend/src/pages/home/HomePage.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import NoteCard from '../../entities/note/ui/NoteCard.vue'
 import Sidebar from '../../shared/ui/Sidebar.vue'
-import { onMounted, ref } from 'vue'
+import { onMounted } from 'vue'
 import { useNotesStore } from '../../entities/note/model/store/notes'
 import { useUiStore } from '../../app/store/uiStore'
-
-const menuIcon = '/src/assets/images/sidebar/menuIcon.svg'
+import BaseButton from '../../shared/ui/BaseButton.vue'
+import router from '../../app/router'
 
 const notesStore = useNotesStore()
 const uiStore = useUiStore()
@@ -14,30 +14,59 @@ onMounted(() => {
   notesStore.fetchNotes()
 })
 
-const isSidebarOpen = ref(false)
+const createNoteAndRedirect = async () => {
+  const newNote = await notesStore.createNote({
+    title: 'New note',
+    content: 'empty',
+    isPinned: false,
+    isArchived: false,
+    tags: []
+  })
+  
+  if (newNote) {
+    router.push({ name: 'note-details', params: { id: newNote.id } })
+  }
+}
 </script>
 
 <template>
-  <div class="md:flex">
-    <Sidebar 
-      class="h-full w-64 transition-transform duration-300 z-30 fixed left-0 md:static md:translate-x-0 pr-4 pl-4" 
-      :class="{
-        'translate-x-0 w-full': uiStore.isSidebarOpen,
-        '-translate-x-full': !uiStore.isSidebarOpen,
-      }"
-    />
+  <div class="flex">
+    <Sidebar />
     
-    <main
-      class="flex-1 p-2"
-      :class="{'hidden': isSidebarOpen}"
-    >
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
-        <NoteCard 
-          v-for="note in notesStore.notes" 
-          :key="note.id" 
-          :note="note" 
-        />
-      </div>
-    </main>
+    <div class="flex-1 flex-col p-2" :class="{'hidden': uiStore.isSidebarOpen}">
+      <BaseButton
+        @click="createNoteAndRedirect"
+        :primary="true"
+        :loading="notesStore.loading"
+        class="h-10 w-full"
+      >
+        Create a note
+      </BaseButton>
+
+      <main
+        class="pb-2 pt-2"
+      >
+        <div v-if="notesStore.loading">
+          <p>Loading notes...</p>
+        </div>
+      
+        <div v-else-if="!notesStore.hasNotes">
+          <p>You don't have any notes yet.</p>
+          <p>Create your first note.</p>
+        </div>
+      
+        <div v-else class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+          <NoteCard 
+            v-for="note in notesStore.sortedNotes" 
+            :key="note.id" 
+            :note="note" 
+          />
+        </div>
+      
+        <div v-if="notesStore.error">
+          <p class="text-red-500">Error: {{ notesStore.error }}</p>
+        </div>
+      </main>
+    </div>
   </div>
 </template>
