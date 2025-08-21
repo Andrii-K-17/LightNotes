462474432d06e960f547a05b966707e5bb4b5f10
Andrii-K-17 <andrii.klodchyk@gmail.com>
2025-08-21 23:03:38 +0300

    feat(frontend): create Message component

---
+++ frontend/src/shared/ui/Message.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  message: string 
+}>() 
+
+</script>
+
+<template>
+  <div v-if="message" class="fixed top-5 left-1/2 -translate-x-1/2 p-4 px-8 bg-neutral-800 text-neutral-100 dark:bg-neutral-200 dark:text-neutral-900 rounded-md shadow-lg z-50">
+    {{ message }}
+  </div>
+</template>
