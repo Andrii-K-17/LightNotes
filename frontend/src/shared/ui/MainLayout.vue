ec9d2aa47ce9ed17d3d5abcf652002d9c50b5b65
Andrii-K-17 <andrii.klodchyk@gmail.com>
2025-08-22 11:19:03 +0300

    feat(frontend): add reminder check on MainLayout load

--- frontend/src/shared/ui/MainLayout.vue
+++ frontend/src/shared/ui/MainLayout.vue
@@ -1,6 +1,37 @@
 <script setup lang="ts">
 import Header from './Header.vue'
 import Footer from './Footer.vue'
+import { onMounted, onUnmounted, ref } from 'vue'
+import { useNotesStore } from '../../entities/note/model/store/notes'
+
+const notesStore = useNotesStore()
+
+const timer = ref<number | null>(null)
+
+const checkReminders = () => {
+  const now = new Date()
+  notesStore.reminderNotes.forEach(async note => {
+    if (note.reminderAt && new Date(note.reminderAt) <= now) {
+      alert(`Reminder: ${note.title} - ${note.content}`)
+      await notesStore.updateNote(note.id, {
+        ...note,
+        reminderAt: null
+      })
+    }
+  }) 
+}
+
+onMounted(async () => {
+  await notesStore.fetchNotes()
+  timer.value = setInterval(checkReminders, 50000) 
+  checkReminders()
+})
+
+onUnmounted(() => {
+  if (timer.value) {
+    clearInterval(timer.value) 
+  }
+}) 
 </script>
 
 <template>
